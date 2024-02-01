bc0682de91559d6108766f3a55fb800b5eacc766
L <globe_909@hotmail.com>
2024-02-01 13:58:48 +0100

    prøver å lage components

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return(
+        <header className="bg-blue-500 p-4 flex justify-between items-center">
+            <h1 className="text-white text-2xl">Værste gjesteparking</h1>
+            <h3 className="text-white text-sm">brukernavn</h3>
+        </header>
+    )
+
+}
---
+++ src/Components/Parking.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Parking.css';
+
+function Parking() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Herlo, Luan sin branch</p>
+        <p>tester</p>
+      </header>
+    </div>
+  );
+}
+
+export default Parking;
