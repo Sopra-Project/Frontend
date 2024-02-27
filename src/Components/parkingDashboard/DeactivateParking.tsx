4a4fe774b3c26c0da133fd644aae5c046c870f70
L <globe_909@hotmail.com>
2024-02-27 16:12:25 +0100

    updating

--- src/Components/parkingDashboard/DeactivateParking.tsx
+++ src/Components/parkingDashboard/DeactivateParking.tsx
@@ -43,12 +43,12 @@ function DeactivateParking({ showModal, setShowModal, handleDeactivateParking, h
             <div className="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center">
                 <h2 className="text-2xl font-bold mb-4">Deaktiver parking</h2>
                 <p className="mt-2 mb-2 text-center">Er du sikker på at du vil deaktivere parkering?</p>
-                <p className="mt-2 mb-2">{parkingRegistrationNumber}</p>
+                <p className="mt-2 mb-6">Registreringsnr: <b> {parkingRegistrationNumber} </b></p>
                 <div className="flex justify-between">
-                    <button className="bg-gray-400 text-white px-4 py-2 rounded-md m-2" onClick={handleDeactivateClick}>
+                    <button className="bg-gray-700 hover:bg-gray-600 text-white px-4 py-2 rounded-md m-2" onClick={handleDeactivateClick}>
                         Deaktiver
                     </button>
-                    <button className="bg-gray-400 text-white px-4 py-2 rounded-md m-2" onClick={handleCloseClick}>
+                    <button className="bg-gray-700 hover:bg-gray-600 text-white px-4 py-2 rounded-md m-2" onClick={handleCloseClick}>
                         Lukk vindu
                     </button>
                 </div>
