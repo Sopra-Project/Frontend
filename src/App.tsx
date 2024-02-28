507a691468300ed26e4047d01981c74325d79416
benjaminhaaaland <73615746+benjaminhaaaland@users.noreply.github.com>
2024-02-28 15:41:08 +0100

    added createuser
    added createuser

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Nav from './Components/Nav';
 import Dashboard from './Components/dashboard/Dashboard';
 import ActivateParking from './Components/parkingDashboard/Activateparking';
 import {SuperAdminDashboard} from "./Components/superadminDashboard/SuperAdminDashboard";
-
+import CreateUser from './Components/dashboard/CreateUser';
 function App() {
     return (
         <Router>
@@ -18,6 +18,7 @@ function App() {
                 <Nav/>
                 <Routes>
                     <Route path="/activeparking" element={<ActivateParking/>}/>
+                    <Route path="/create-user" element={<CreateUser />} />
                     <Route path="/" element={<ParkingIssuer/>}/>
                     <Route path="/login" element={<Login/>}/>
                     <Route path="/deactivateparking/:id" element={<DeactivateParking/>}/>
