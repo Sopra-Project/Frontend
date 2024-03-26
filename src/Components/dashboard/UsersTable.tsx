7b10bf01b0f795ffc989bd33529dbc038389d05d
benjaminhaaaland <73615746+benjaminhaaaland@users.noreply.github.com>
2024-03-26 14:03:51 +0100

    updated again for better responsitivity
    updated again for better responsitivity

--- src/Components/dashboard/UsersTable.tsx
+++ src/Components/dashboard/UsersTable.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons'; // Importer "edit" ikonet
+import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
 import { User } from '../../types/types';
 import AdminService from "../../services/AdminService";
 
@@ -23,7 +23,7 @@ const UsersTable = () => {
                 setError(error.message);
                 setLoading(false);
             });
-    }, [users]);
+    }, []);
 
     const handleCreateUserClick = () => setIsModalOpen(true);
 
@@ -36,10 +36,8 @@ const UsersTable = () => {
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-
         const roleId = selectedRole === 'admin' ? 1 : 2;
         const userData = { name, email, roleId };
-
         try {
             const response = await AdminService.createUser(userData);
             if (response.ok) {
@@ -58,58 +56,56 @@ const UsersTable = () => {
 
     return (
         <div className="overflow-x-auto pb-5 pt-5 shadow-xl bg-gray-100">
-            {/* Kortoppsett for mindre skjermer */}
             <div className="md:hidden">
                 {users.map((user) => (
-                    <div key={user.id} className="bg-white shadow overflow-hidden rounded-lg mb-4 p-4">
-                        <div><strong>Navn:</strong> {user.name}</div>
+                    <div key={user.id} className="p-4 m-4 bg-white rounded-lg shadow-lg">
+                        <div><strong>Name:</strong> {user.name}</div>
                         <div><strong>Email:</strong> {user.email}</div>
-                        <div><strong>Rolle:</strong> {user.role.authority}</div>
-                        <div className="flex justify-end space-x-2 mt-2">
-                            <button onClick={() => { /* Implementer redigeringsfunksjonalitet */ }} className="text-white bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded">
+                        <div><strong>Role:</strong> {user.role.authority}</div>
+                        <div className="flex justify-end space-x-2 mt-4">
+                            <button className="text-blue-500 hover:text-blue-700">
                                 <FontAwesomeIcon icon={faEdit} />
                             </button>
-                            <button onClick={() => { /* Implementer slettefunksjonalitet */ }} className="text-white bg-red-500 hover:bg-red-700 font-bold py-2 px-4 rounded">
+                            <button className="text-red-500 hover:text-red-700">
                                 <FontAwesomeIcon icon={faTrash} />
                             </button>
                         </div>
                     </div>
                 ))}
             </div>
-
-            <div className="w-full overflow-hidden sm:rounded-md shadow-xl">
-                <table className="w-full bg-white divide-y divide-gray-200 rounded-md">
-                    <thead style={{backgroundColor: '#F3F2EE'}} className="shadow-3xl">
-                    <tr>
-                        <th className="px-6 py-3 p-4 text-sm font-semibold tracking-wide text-left">Navn</th>
-                        <th className="px-6 py-3 p-4 text-sm font-semibold tracking-wide text-left">Email</th>
-                        <th className="px-6 py-3 p-4 text-sm font-semibold tracking-wide text-left">Rolle</th>
-                        <th className="px-6 py-3 p-4 text-sm font-semibold tracking-wide text-left">Handlinger</th>
-                    </tr>
-                    </thead>
-                    <tbody className="bg-white divide-y divide-gray-200">
-                    {users.map((user) => (
-                        <tr key={user.id} className="hover:bg-gray-100">
-                            <td className="p-4 text-sm text-gray-700 whitespace-nowrap">{user.name}</td>
-                            <td className="p-4 text-sm text-gray-700 whitespace-nowrap">{user.email}</td>
-                            <td className="p-4 text-sm text-gray-700 whitespace-nowrap">{user.role.authority}</td>
-                            <td className="p-4 text-sm text-gray-700 whitespace-nowrap">
-                                <button onClick={() => { /* Handle edit */ }} className="px-6 py-3 p-4 text-sm font-semibold tracking-wide text-left">
-                                    <FontAwesomeIcon icon={faEdit} className="mx-2"/> {/* Legg til edit ikonet */}
-                                </button>
-                                <button onClick={() => { /* Handle delete */ }} className="bg-red-700 hover:bg-red-800 text-white py-2 px-3 rounded-md">
-                                    <FontAwesomeIcon icon={faTrash} className="mx-2"/>
-                                </button>
-                            </td>
+            <div className="hidden md:block">
+                <div className="overflow-x-auto">
+                    <table className="min-w-full divide-y divide-gray-200">
+                        <thead style={{backgroundColor: '#F3F2EE'}} className="shadow-3xl">
+                        <tr>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Navn</th>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Rolle</th>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Handlinger</th>
                         </tr>
-                    ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody className="bg-white divide-y divide-gray-200">
+                        {users.map((user) => (
+                            <tr key={user.id}>
+                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{user.name}</td>
+                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.email}</td>
+                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.role.authority}</td>
+                                <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                    <button className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                        <FontAwesomeIcon icon={faEdit}/>
+                                    </button>
+                                    <button className="text-red-600 hover:text-red-900">
+                                        <FontAwesomeIcon icon={faTrash}/>
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-
             <div className="flex justify-end mt-4">
-                <button onClick={handleCreateUserClick}
-                        className="btn text-white font-semibold border-gray-700 transition-colors duration-300 px-8 py-2 rounded-lg border-2 border-white hover:border-gray-500">
+                <button onClick={handleCreateUserClick} className="btn text-white font-semibold border-gray-700 transition-colors duration-300 px-8 py-2 rounded-lg border-2 border-white hover:border-gray-500">
                     Lag ny bruker
                 </button>
             </div>
@@ -133,19 +129,19 @@ const UsersTable = () => {
                                 <label htmlFor="role" className="block text-sm font-medium text-gray-700">Role:</label>
                                 <select id="role" value={selectedRole} onChange={(e) => setSelectedRole(e.target.value)}
                                         className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none">
-                                    <option value="">Select a role</option>
+                                    <option value="">Velg rolle</option>
                                     <option value="admin">Admin</option>
                                     <option value="user">User</option>
                                 </select>
                             </div>
                             <button type="submit"
                                     className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-marine-blue-dark hover:bg-marine-blue">
-                                Create User
+                                Lag bruker
                             </button>
                         </form>
                         <button onClick={handleCloseModal}
                                 className="mt-4 w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-gray-100 hover:bg-gray-200">
-                            Close
+                            Lukk
                         </button>
                     </div>
                 </div>
